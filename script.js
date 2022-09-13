bbf920d9eeee729bef61cd8e73be023d4f1f18bb
Delali97 <jennifer.kpolu@yahoo.com>
2022-09-12 23:42:44 -0300

    first init

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation ({
+    container: document.getElementById(firstLottie),
+    render: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets6.lottiefiles.com/packages/lf20_iratqmhn.json'
+})
